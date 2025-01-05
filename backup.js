1772a29986f82cd75ade940d385e190dcb9dfc5a
Mohammed-Akramuddin <mohdakram6174@gmail.com>
2025-01-05 21:58:18 +0530

    Add files via upload

---
+++ backup.js
@@ -0,0 +1,13 @@
+document.querySelector('#resu').innerHTML = `
+<section class="resume-section">
+<h2 class="section-title">Resume</h2>
+<div class="resume-container">
+  <div class="download-card">
+    <a href="https://github.com/Mohammed-Akramuddin/portfolio/raw/main/resume_2.pdf" download class="download-button">
+  <i class="fas fa-download"></i>
+  <span>Download Resume</span>
+      </a>
+  </div>
+</div>
+</section>
+`
